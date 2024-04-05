f8099a75f81af74e8add53a9d2c02dd706ebae24
Noah Lomu <noah.lomu@gmail.com>
2024-04-05 10:44:28 -0600

    Add USART transmission of PWM values
    The calculated PWM values are sent over USART, which is the protocol that will be used with the Bluetooth modules.

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -51,6 +51,10 @@
 void SystemClock_Config(void);
 /* USER CODE BEGIN PFP */
 
+void USART3_TxByte(uint8_t data);
+void USART3_txString(char* data);
+uint8_t USART3_RxByte(void);
+
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -66,15 +70,23 @@ int main(void)
 {
   /* USER CODE BEGIN 1 */
   
-  const int arrValue = 125;
+  const int arrValue = 255;
+  const int targetBaud = 9600;
   
+  int clkSpeed;
   int returnValue;
+  
   int16_t xValue;
   int16_t yValue;
   int16_t zValue;
-  int redPWMPercent;
-  int greenPWMPercent;
-  int bluePWMPercent;
+  
+  uint8_t redPWMValue;
+  uint8_t greenPWMValue;
+  uint8_t bluePWMValue;
+  
+  uint8_t redData[] = {'R', 0};
+  uint8_t greenData[] = {'G', 0};
+  uint8_t blueData[] = {'B', 0};
   
   /* USER CODE END 1 */
 
@@ -97,6 +109,7 @@ int main(void)
   RCC->AHBENR |= RCC_AHBENR_GPIOBEN; // enable GPIO B clock
   RCC->AHBENR |= RCC_AHBENR_GPIOCEN; // enable GPIO C clock
   RCC->APB1ENR |= RCC_APB1ENR_SPI2EN; // enable SPI 2 clock
+  RCC->APB1ENR |= RCC_APB1ENR_USART3EN; // enable USART 3 clock
   
   // Configure GPIO C pins 6, 7, and 8 for PWM LED Strip signal generation
   GPIOC->MODER &= ~(GPIO_MODER_MODER6_Msk | GPIO_MODER_MODER7_Msk | GPIO_MODER_MODER8_Msk);
@@ -107,14 +120,21 @@ int main(void)
   GPIOC->AFR[0] &= ~(GPIO_AFRL_AFSEL6 | GPIO_AFRL_AFSEL7); // alternate function 0
   GPIOC->AFR[1] &= ~(GPIO_AFRH_AFSEL8); // alternate function 0
   
-  // Configure GPIO C pins 6, 7, 8, and 9 (LED pins)
-  /*
-  GPIOC->MODER &= ~(GPIO_MODER_MODER6_Msk | GPIO_MODER_MODER7_Msk | GPIO_MODER_MODER8_Msk | GPIO_MODER_MODER9_Msk);
-  GPIOC->MODER |= (0x1 << GPIO_MODER_MODER6_Pos) | (0x1 << GPIO_MODER_MODER7_Pos) | 
-                  (0x1 << GPIO_MODER_MODER8_Pos) | (0x1 << GPIO_MODER_MODER9_Pos); // output mode
-  GPIOC->OTYPER &= ~(GPIO_OTYPER_OT_6 | GPIO_OTYPER_OT_7 | GPIO_OTYPER_OT_8 | GPIO_OTYPER_OT_9); // push-pull
-  GPIOC->PUPDR &= ~(GPIO_PUPDR_PUPDR6 | GPIO_PUPDR_PUPDR7 | GPIO_PUPDR_PUPDR8 | GPIO_PUPDR_PUPDR9); // no pull-up/pull-down
-  */
+  // Configure GPIO C pins 10 and 11 to connect to USART 3
+  GPIOC->MODER &= ~(GPIO_MODER_MODER10_Msk | GPIO_MODER_MODER11_Msk);
+  GPIOC->MODER |= (0x2 << GPIO_MODER_MODER10_Pos) | (0x2 << GPIO_MODER_MODER11_Pos); // alternate mode
+  GPIOC->OTYPER &= ~(GPIO_OTYPER_OT_10 | GPIO_OTYPER_OT_11); // push-pull
+  GPIOC->OSPEEDR &= ~(GPIO_OSPEEDR_OSPEEDR10 | GPIO_OSPEEDR_OSPEEDR11); // low-speed
+  GPIOC->PUPDR &= ~(GPIO_PUPDR_PUPDR10 | GPIO_PUPDR_PUPDR11); // no pull-up/pull-down
+  GPIOC->AFR[1] &= ~(GPIO_AFRH_AFSEL10_Msk | GPIO_AFRH_AFSEL11_Msk);
+  GPIOC->AFR[1] |= (0x1 << GPIO_AFRH_AFSEL10_Pos) | (0x1 << GPIO_AFRH_AFSEL11_Pos); // alternate function 1
+  
+  // Configure USART 3
+  clkSpeed = HAL_RCC_GetHCLKFreq();
+  USART3->CR1 |= USART_CR1_RE | USART_CR1_TE; // enable TX and RX
+  USART3->CR1 |= USART_CR1_RXNEIE; // enable interrupts from receive register not empty
+  USART3->BRR &= 0x0000;
+  USART3->BRR |= clkSpeed / targetBaud; // set baud rate clock divisor
   
   /* NOTE:
      To connect the gyroscope via SPI, the following pins are used:
@@ -158,7 +178,7 @@ int main(void)
     
   // Set the timer 3 prescaler to 80 to get to 10us resolution
   TIM3->PSC &= 0x0;
-  TIM3->PSC |= 79;
+  TIM3->PSC |= 38;
   
   // Set the timer 3 auto reload point to 125 to get to 800Hz
   TIM3->ARR &= 0x0;
@@ -179,26 +199,19 @@ int main(void)
   // Enable Capture/Compare for channels 1, 2, and 3
   TIM3->CCER |= TIM_CCER_CC1E | TIM_CCER_CC2E | TIM_CCER_CC3E;
   
-  // Set the compare registers to 100%, 20%, and 60%
-  TIM3->CCR1 &= 0x0000;
-  TIM3->CCR1 |= (100*arrValue)/100;
-  TIM3->CCR2 &= 0x0000;
-  TIM3->CCR2 |= (20*arrValue)/100;
-  TIM3->CCR3 &= 0x0000;
-  TIM3->CCR3 |= (60*arrValue)/100;
-  
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
   /* USER CODE BEGIN 2 */
   
   TIM3->CR1 |= TIM_CR1_CEN; // enable timer 3
+  USART3->CR1 |= USART_CR1_UE; // enable USART 3
   
   GPIOC->ODR |= GPIO_ODR_0; // set pin C0 high when SPI is not ongoing
   SPI2->CR1 |= SPI_CR1_SPE; // enable SPI 2
   
-  returnValue = I3G4250D_ReadRegister(I3G4250D_WHO_AM_I_Addr, 1); // Read WHO_AM_I
-  if (returnValue != I3G4250D_WHO_AM_I_Value) // Verify that the gyroscope matches the known WHO_AM_I
+  returnValue = I3G4250D_ReadRegister(I3G4250D_WHO_AM_I_Addr, 1); // read WHO_AM_I
+  if (returnValue != I3G4250D_WHO_AM_I_Value) // verify that the gyroscope matches the known WHO_AM_I
   {
     Error_Handler();
   }
@@ -208,9 +221,9 @@ int main(void)
                             I3G4250D_CTRL_REG1_Zen | I3G4250D_CTRL_REG1_PD)
                           ); // enable X, Y, Z, and normal mode
   
-  redPWMPercent = 0;
-  greenPWMPercent = 0;
-  bluePWMPercent = 0;
+  redPWMValue = 0;
+  greenPWMValue = 0;
+  bluePWMValue = 0;
   
   /* USER CODE END 2 */
 
@@ -230,66 +243,52 @@ int main(void)
     // *****************************************************
     // **** gyro output to brightness modulation logic *****
     // *****************************************************
-    if (xValue > 4000) // x positive
+    if (xValue > 4000 && redPWMValue < arrValue) // x positive
     {
-      redPWMPercent++; // increase red brightness
-      if (redPWMPercent > arrValue)
-      {
-        redPWMPercent = arrValue;
-      }
+      redPWMValue++; // increase red brightness
     }
-    else if (xValue < -4000) // x negative
+    else if (xValue < -4000 && redPWMValue > 0) // x negative
     {
-      redPWMPercent--; // decrease red brightness
-      if (redPWMPercent < 0)
-      {
-        redPWMPercent = 0;
-      }
+      redPWMValue--; // decrease red brightness
     }
     
-    if (yValue > 4000) // y positive
+    if (yValue > 4000 && greenPWMValue < arrValue) // y positive
     {
-      greenPWMPercent++; // increase green brightness
-      if (greenPWMPercent > arrValue)
-      {
-        greenPWMPercent = arrValue;
-      }
+      greenPWMValue++; // increase green brightness
     }
-    else if (yValue < -4000) // y negative
+    else if (yValue < -4000 && greenPWMValue > 0) // y negative
     {
-      greenPWMPercent--; // decrease green brightness
-      if (greenPWMPercent < 0)
-      {
-        greenPWMPercent = 0;
-      }
+      greenPWMValue--; // decrease green brightness
     }
     
-    if (zValue > 4000) // z positive
+    if (zValue > 4000 && bluePWMValue < arrValue) // z positive
     {
-      bluePWMPercent++; // increase blue brightness
-      if (bluePWMPercent > arrValue)
-      {
-        bluePWMPercent = arrValue;
-      }
+      bluePWMValue++; // increase blue brightness
     }
-    else if (zValue < -4000) // z negative
+    else if (zValue < -4000 && bluePWMValue > 0) // z negative
     {
-      bluePWMPercent--; // decrease blue brightness
-      if (bluePWMPercent < 0)
-      {
-        bluePWMPercent = 0;
-      }
+      bluePWMValue--; // decrease blue brightness
     }
+    
+    // Assign PWM values to data to send over USART 3
+    redData[1] = redPWMValue;
+    greenData[1] = greenPWMValue;
+    blueData[1] = bluePWMValue;
+    
+    // Send color data over USART 3
+    USART3_txString((char*)redData);
+    USART3_txString((char*)greenData);
+    USART3_txString((char*)blueData);
     
     /* NOTE: Make sure the colors here match the LED strip
        They do not match right now
      */
     TIM3->CCR1 &= 0x0000;
-    TIM3->CCR1 |= (redPWMPercent*arrValue)/100;
+    TIM3->CCR1 |= redPWMValue;
     TIM3->CCR2 &= 0x0000;
-    TIM3->CCR2 |= (greenPWMPercent*arrValue)/100;
+    TIM3->CCR2 |= greenPWMValue;
     TIM3->CCR3 &= 0x0000;
-    TIM3->CCR3 |= (bluePWMPercent*arrValue)/100;
+    TIM3->CCR3 |= bluePWMValue;
     
     HAL_Delay(20);
   }
@@ -399,6 +398,37 @@ void I3G4250D_WriteToRegister(uint8_t registerAddr, uint8_t data)
   while (SPI2->SR & SPI_SR_RXNE) (void)SPI2->DR; // clear the receive buffer
   
   return;
+}
+
+/**
+  * @brief Transmits a byte via USART 3
+  */
+void USART3_TxByte(uint8_t data)
+{
+  while ((USART3->ISR & (0x1 << 7)) == 0x0);
+  USART3->TDR = data;
+  return;
+}
+
+/**
+  * @brief Transmits a string via USART 3
+  */
+void USART3_txString(char* data)
+{
+  for (int i = 0; data[i] != '\0'; i++)
+  {
+    USART3_TxByte((uint8_t)data[i]);
+  }
+  return;
+}
+
+/**
+  * @brief Receives a byte via USART 3
+  */
+uint8_t USART3_RxByte(void)
+{
+  while ((USART3->ISR & (0x1 << 5)) == 0x0);
+  return USART3->RDR;
 }
 
 /* USER CODE END 4 */
